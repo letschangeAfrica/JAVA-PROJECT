8c27ceed6599cc2fd1e4afba71fb6ced2c003e3c
Ismaël KAGOU <127965613+letschangeAfrica@users.noreply.github.com>
2025-06-15 10:56:43 +0200

    Add files via upload
    Uploaded my pkfemployment project on github

---
+++ PkfEmployeeManagementSystem/src/pkfemployeemanagementsystem/getData.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkfemployeemanagementsystem;
+
+/**
+ *
+ * @author Charity
+ */
+public class getData {
+     public static String username;
+    public static String path;
+}
